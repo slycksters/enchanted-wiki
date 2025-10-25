3a2e483a65388e5ffa42c9f904b2b3f1d868e9ec
Ron Madrid <ron.madrid@op2mise.ai>
2025-10-26 04:12:03 +0800

    edited AbilitiesPage

--- src/pages/abilities/AbilitiesPage.jsx
+++ src/pages/abilities/AbilitiesPage.jsx
@@ -1,9 +1,11 @@
 import styles from './Abilities.module.css';
+import { WorkInProgressPage } from '../utilities';
 
 export const AbilitiesPage = () => {
   return (
-    <div className={styles.abilitiesPageContainer}>
-      <h1>Abilities</h1>
-    </div>
+    // <div className={styles.abilitiesPageContainer}>
+    //   <h1>Abilities</h1>
+    // </div>
+    <WorkInProgressPage />
   )
 };
