c144825560b2b049d7ccb0d802da9e41480e4b30
Ron Madrid <ron.madrid@op2mise.ai>
2025-10-27 20:43:08 +0800

    fixed scrollbar issue in front page, added new color theme but unused for now

--- src/components/layout/MainComponent.jsx
+++ src/components/layout/MainComponent.jsx
@@ -6,11 +6,16 @@ export const MainComponent = () => {
   const location = useLocation();
   const showNavigation =
     location.pathname !== BASE_PATH && location.pathname !== `${BASE_PATH}/`;
+  const showLineGraphBackground =
+    location.pathname !== BASE_PATH && location.pathname !== `${BASE_PATH}/`;
 
   return (
     <div>
       {showNavigation && <NavigationComponent />}
-      <div style={{ height: 'calc(100vh - 80px)' }}>
+      <div
+        className={showLineGraphBackground ? 'line-graph-background' : ''}
+        style={{ height: showNavigation ? 'calc(100vh - 80px)' : '' }}
+      >
         <Outlet />
       </div>
     </div>
