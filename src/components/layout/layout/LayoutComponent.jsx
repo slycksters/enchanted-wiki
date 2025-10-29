d1fffe1f1ef84b3dd83804ea5ff5d6a639ac5acb
Ron Madrid <ron.madrid@op2mise.ai>
2025-10-29 09:53:44 +0800

    fixed issues with scrolling to elements, element heights and cleaned up unnecessary stylings

--- src/components/layout/layout/LayoutComponent.jsx
+++ src/components/layout/layout/LayoutComponent.jsx
@@ -16,22 +16,27 @@ const ChildComponent = ({ children, selectedCategory, setSelectedInfo }) => {
 };
 
 export const LayoutComponent = ({ categories, children, defaultInfo }) => {
+  const isMobileOrTablet = window.innerWidth <= 1024;
   const [selectedInfo, setSelectedInfo] = useState(defaultInfo);
   const [showSidebar, setShowSidebar] = useState(false);
   const [showInfoPanel, setShowInfoPanel] = useState(false);
 
   // --- handle click on sidebar's items
   const handleScrollToSection = (item) => {
-    const section = document.getElementById(`${item.type.name}-${item.id}-${item.name}`);
+    const section = document.getElementById(
+      `${item.type.name}-${item.id}-${item.name}`
+    );
     if (section) {
       section.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
+
+    if (isMobileOrTablet) setShowSidebar(false);
   };
 
   // --- handle click on child component's cards
   const handleSelectInfo = (info) => {
     setSelectedInfo(info);
-    if (window.innerWidth <= 1024) {
+    if (isMobileOrTablet) {
       setShowInfoPanel(true);
     }
   };
@@ -50,10 +55,7 @@ export const LayoutComponent = ({ categories, children, defaultInfo }) => {
       />
 
       {/* Content */}
-      <ChildComponent
-        children={children}
-        setSelectedInfo={handleSelectInfo}
-      />
+      <ChildComponent children={children} setSelectedInfo={handleSelectInfo} />
 
       {/* Info Panel */}
       <InfoPanelComponent
